2135d2c258404fc71d859be608e258b4cfb9becc
hyeon <w2122@e-mirim.hs.kr>
2023-06-29 13:03:45 +0900

    어려버...

--- color/models.py
+++ color/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Colors(models.Model):
+    color_name = models.CharField(max_length=20)
+    color_image = models.ImageField(upload_to='color_images/%Y/%m/%d/', null=True, blank=True)
+    color_ex = models.TextField
+
--- color/views.py
+++ color/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from color.models import Colors
 # Create your views here.
+
